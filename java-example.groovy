8323785c6aaea60d0c9da649c363565f79c80c93
Raymond Vargas <vargasraymondj@gmail.com>
2019-12-09 21:15:13 -0500

    initial commit

---
+++ java-example.groovy
@@ -0,0 +1,23 @@
+mavenJob('DNF-API') { // Name of the Jenkins job
+    jdk('Java 8')
+    scm {
+        github('git://github.com/VargasRaymondJ/DNF2018-API')
+    }
+    triggers {
+        githubPush()
+    }
+    goals('clean')
+    goals('install')
+}
+
+// mavenJob('example') {
+//     logRotator(-1, 10)
+//     jdk('Java 8')
+//     scm {
+//         github('jenkinsci/jenkins', 'master')
+//     }
+//     triggers {
+//         githubPush()
+//     }
+//     goals('clean verify')
+// }
