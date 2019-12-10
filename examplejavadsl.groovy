8dd0926fca915df6224aaa62e7eeac1e930551bb
Raymond Vargas <vargasraymondj@gmail.com>
2019-12-10 10:35:00 -0500

    comments

--- examplejavadsl.groovy
+++ examplejavadsl.groovy
@@ -1,5 +1,8 @@
+
+// Maven had to be manually installed on the box
+// This DSL script relies on a particular plugin
 mavenJob('DNF-API') { // Name of the Jenkins job
-    jdk('java 8')
+    jdk('java 8') // This had to be manually installed on the box
     scm {
         github('VargasRaymondJ/DNF2018-API')
     }
